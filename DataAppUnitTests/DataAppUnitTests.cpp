80b3d4063ed552e6e70b93da3ee5f54212fb9f1c
coolnick1999 <coolnick1999@gmail.com>
2021-03-11 22:36:58 +0700

    DataAppUnitTest project created

---
+++ DataAppUnitTests/DataAppUnitTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DataAppUnitTests
+{
+	TEST_CLASS(DataAppUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
